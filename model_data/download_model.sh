dca9400f73863827db08ce8334d61a6e3b3f568d
busyyang <jzhm_4@hotmail.com>
2021-08-07 09:01:58 +0800

    update model download url

--- model_data/download_model.sh
+++ model_data/download_model.sh
@@ -1,1 +1,3 @@
-wget https://github.com/busyyang/pi-face/releases/download/facenet_keras_model/facenet_keras.h5 
+wget https://github.com/busyyang/pi-face/releases/download/release_model/facenet_keras.h5 
+wget https://github.com/busyyang/pi-face/releases/download/release_model/facenet.tflite 
+wget https://github.com/busyyang/pi-face/releases/download/release_model/facenet_quantize.tflite 
