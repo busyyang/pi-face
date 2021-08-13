5be2c682d062b7586bf4337c9e31943ca6d4423c
jyang <jyang@wisepool.com.cn>
2021-08-13 15:22:03 +0800

    删除文件

--- model_data/download_model.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/busyyang/pi-face/releases/download/release_model/facenet_keras.h5 
-wget https://github.com/busyyang/pi-face/releases/download/release_model/facenet.tflite 
-wget https://github.com/busyyang/pi-face/releases/download/release_model/facenet_quantize.tflite 
